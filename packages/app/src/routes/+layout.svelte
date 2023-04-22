3890bebf82399201c8d563ef45e1bc8e2b44b44c
Kenk <kenghin_lim@hotmail.com>
2023-04-22 14:49:39 +0800

    fix: update payment indexer

--- packages/app/src/routes/+layout.svelte
+++ packages/app/src/routes/+layout.svelte
@@ -15,61 +15,56 @@
   import type { Payment } from "../types";
   import { parseEther } from "ethers/lib/utils.js";
   import { orders } from "../stores";
-
   import { getNetwork } from "@wagmi/core";
-
   // Listen for PaymentReceived Events
   wagmiClient.subscribe((client) => {
     if (client) {
-      console.log("Client is ready");
-      let { chain, chains } = getNetwork();
-      console.log("🚀 | wagmiClient.subscribe | chains:", chains);
-      console.log("🚀 | wagmiClient.subscribe | chain:", chain);
+      let { chain } = getNetwork();
+      console.log("🚀 | wagmiClient.subscribeee | chain:", chain);
+      if (chain) {
+        // Only Start Listening WHen Client is Ready
+        watchAutopassPaymentGatewayEvent(
+          { eventName: "PaymentReceived", chainId: chain.id as 31337 },
+          async (sender, amount, orderId, paymentId, data) => {
+            console.log({
+              sender,
+              amount: amount.toString(),
+              orderId,
+              paymentId: paymentId.toString(),
+              data,
+            });
 
-      // Only Start Listening WHen Client is Ready
-      watchAutopassPaymentGatewayEvent(
-        { eventName: "PaymentReceived" },
-        async (sender, amount, orderId, paymentId, data) => {
-          console.log({
-            sender,
-            amount: amount.toString(),
-            orderId,
-            paymentId: paymentId.toString(),
-            data,
-          });
+            // Update backend
+            // POST api/payment
+            let payment: Payment = {
+              userAddress: sender,
+              orderId: orderId,
+              amount: amount,
+            };
+            const response = await fetch("/api/payment", {
+              method: "POST",
+              body: JSON.stringify(payment),
+              headers: {
+                "content-type": "application/json",
+              },
+            });
 
-          // Update backend
-          // POST api/payment
-          let payment: Payment = {
-            userAddress: sender,
-            orderId: orderId,
-            amount: amount,
-          };
-          const response = await fetch("/api/payment", {
-            method: "POST",
-            body: JSON.stringify(payment),
-            headers: {
-              "content-type": "application/json",
-            },
-          });
-
-          // get orders and update store
-          const getResponse = await fetch("/api/order", {
-            method: "GET",
-            headers: {
-              "content-type": "application/json",
-            },
-          });
-          let _orders = await getResponse.json();
-          console.log("🚀 | handleClick | _orders:", _orders);
-          let _ordersKeys = Object.keys(_orders);
-          $orders = _ordersKeys.map((key) => _orders[key]);
-        }
-      );
+            // get orders and update store
+            const getResponse = await fetch("/api/order", {
+              method: "GET",
+              headers: {
+                "content-type": "application/json",
+              },
+            });
+            let _orders = await getResponse.json();
+            console.log("🚀 | handleClick | _orders:", _orders);
+            let _ordersKeys = Object.keys(_orders);
+            $orders = _ordersKeys.map((key) => _orders[key]);
+          }
+        );
+      }
     }
   });
-
-  onMount(async () => {});
 </script>
 
 <!-- Comment to Disable WEB3 (Requires VITE_WEB3MODAL_PROJECT_ID to work) -->
